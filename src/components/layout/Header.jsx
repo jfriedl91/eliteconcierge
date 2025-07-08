a4493e7c9a201c0e5c03949ff6f69dfcc9178051
Johannes Friedl <info@elconci.de>
2025-07-09 00:11:56 +0200

    ref:rev5

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -93,12 +93,12 @@ const Header = () => {
 <div className="w-full bg-gold/90 text-background flex items-center justify-center py-2 px-4 shadow z-40" style={{ marginTop: 80 }}>
   <Phone className="w-5 h-5 mr-2" />
   <span className="font-semibold tracking-wide">
-    Unsere neue Servicerufnummer:&nbsp;
+    24/7 Exzellenz - Unser Concierge Team steht Ihnen jederzeit zur Verfügung:&nbsp;
     <a
       href="tel:0800123456789"
       className="underline hover:text-platinum transition-colors duration-200"
     >
-      0800 123456789
+      06102 574 62 44 (Ortstarif)
     </a>
   </span>
 </div>
