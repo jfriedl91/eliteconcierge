92e70def9647b289632074ae5193fe35c029aa8b
Johannes Friedl <info@elconci.de>
2025-07-08 23:29:51 +0200

    ref:restore

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,6 @@ import PageLoader from '@/components/PageLoader';
 import AdminLayout from '@/components/layout/AdminLayout';
 import MemberLayout from '@/components/layout/MemberLayout';
 
- {/* Banner GANZ OBEN */}
-      <div className="w-full bg-gold/90 text-background flex items-center justify-center py-2 px-4 shadow-lg z-30">
-        <Phone className="w-5 h-5 mr-2" />
-        <span className="font-semibold tracking-wide">
-          Unsere neue Servicerufnummer:&nbsp;
-          <a
-            href="tel:0800123456789"
-            className="underline hover:text-platinum transition-colors duration-200"
-          >
-            0800 123456789
-          </a>
-        </span>
-      </div>
-
 const HomePage = lazy(() => import('@/pages/Home'));
 const AboutPage = lazy(() => import('@/pages/About'));
 const ServicesPage = lazy(() => import('@/pages/Services'));
