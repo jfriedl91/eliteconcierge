7597ccaeea4e5a684d094e90cb7522fed7fcf01b
Johannes Friedl <info@elconci.de>
2025-07-08 23:25:41 +0200

    ref:backup

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ function App() {
 
   return (
     <>
-     <div className="w-full bg-gold/90 text-background flex items-center justify-center py-2 px-4 shadow-lg z-30">
+      <div className="w-full bg-gold/90 text-background flex items-center justify-center py-2 px-4 shadow-lg z-30">
         <Phone className="w-5 h-5 mr-2" />
         <span className="font-semibold tracking-wide">
           Unsere neue Servicerufnummer:&nbsp;
