140208207c92bce62d8ae01aa6f254a711c08bc9
Johannes Friedl <info@elconci.de>
2025-07-26 12:08:03 +0200

    ref: update meta titel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -30,7 +30,7 @@ const Home = () => {
   return (
     <PageWrapper>
       <Helmet>
-        <title>Elite Concierge: Premium privater und Business Concierge Service</title>
+        <title>Elite Concierge-Privat/Business-Concierge:persönlicher Service </title>
         <meta
           name="description"
           content="Exklusiver Concierge-Service. Ihr Leben. Unser Service. Ihre Zeit zählt. Buchen Sie noch heute Ihre Erstberatung."
